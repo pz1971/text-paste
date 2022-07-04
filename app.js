3b2d8382c51c91c66e82fdaad648d909b8c742c4
Parvez Mia <42247008+pz1971@users.noreply.github.com>
2022-07-05 01:02:45 +0600

    received data in backend

--- app.js
+++ app.js
@@ -1,21 +1,36 @@
 import express from 'express';
-import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 
-const app = express();
-app.use(bodyParser.json());
-
+// defining the port
 const port = process.env.PORT || 5000;
+
+// express app
+const app = express();
 
 // using ejs as the view engine
 app.set('view engine', 'ejs');
+
+// url encoded data parser
+app.use(express.urlencoded({ extended: false }))
 
 // use '/public' as the static folder
 app.use(express.static('public'));
 
 // render home page
-app.get('/', async (req, res) => {
+app.get('/', (req, res) => {
     res.render('index');
+})
+
+app.post('/', (req, res) => {
+    // get the data from the form
+    var text_type = req.body.text_type;
+    var expire_option = req.body.expire_option;
+    var pass_protected = req.body.pass_protected;
+    var password = req.body.password;
+    var text = req.body.text;
+
+    // TODO: store data in database
+    // TODO: redirect to the pasted page
 })
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
