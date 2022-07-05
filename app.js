d79aea4dd655f839ab96e137f339966f3dcb6d98
Parvez Mia <42247008+pz1971@users.noreply.github.com>
2022-07-05 12:18:07 +0600

    mongodb connected

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import './env.js';
 
-// defining the port
-const port = process.env.PORT || 5000;
+// connect to mongoDB
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true, useUnifiedTopology: true
+}).then(() => {
+    console.log('Connected to MongoDB');
+}).catch(err => {
+    console.log('Error Connecting MongoDB:', err.message);
+});
+
+// port no.
+const port = process.env.PORT;
 
 // express app
 const app = express();
